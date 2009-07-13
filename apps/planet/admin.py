301c1f856d309c2d3c59e589e51904836fea27f4
Agustín Méndez <matagus@gmail.com>
2009-07-12 22:57:59 -0300

    Add: Custom AdminModel for each Model.

--- apps/planet/admin.py
+++ apps/planet/admin.py
@@ -5,7 +5,60 @@ from django.contrib import admin
 from planet.models import (Blog, Generator, Feed, FeedLink, Post, PostLink,
     Author, PostAuthorData, Enclosure)
 
-for model in (Blog, Generator, Feed, FeedLink, Post, PostLink, Author,
-        PostAuthorData, Enclosure):
-    admin.site.register(model)
 
+class PostLinkAdmin(admin.ModelAdmin):
+    list_display = ("title", "rel", "mime_type", "post", "link")
+    list_filter = ("rel", "mime_type")
+
+admin.site.register(PostLink, PostLinkAdmin)
+
+
+class PostAuthorDataAdmin(admin.ModelAdmin):
+    list_display = ("author", "is_contributor", "post")
+    list_filter = ("is_contributor", "author")
+
+admin.site.register(PostAuthorData, PostAuthorDataAdmin)
+
+
+class EnclosureAdmin(admin.ModelAdmin):
+    list_display = ("post", "mime_type", "length", "link")
+    list_filter = ("mime_type", )
+
+admin.site.register(Enclosure, EnclosureAdmin)
+
+class FeedAdmin(admin.ModelAdmin):
+    list_display = ("title", "url", "blog", "language", "generator")
+    list_filter = ("language", "generator", )
+
+admin.site.register(Feed, FeedAdmin)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("name", "email")
+
+admin.site.register(Author, AuthorAdmin)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "feed", "url")
+    list_filter = ("feed", )
+
+admin.site.register(Post, PostAdmin)
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ("title", "url")
+
+admin.site.register(Blog, BlogAdmin)
+
+
+class GeneratorAdmin(admin.ModelAdmin):
+    list_display = ("name", "version", "link")
+
+admin.site.register(Generator, GeneratorAdmin)
+
+
+class FeedLinkAdmin(admin.ModelAdmin):
+    list_display = ("feed", "mime_type", "rel", "link")
+    list_filter = ("mime_type", "rel")
+
+admin.site.register(FeedLink, FeedLinkAdmin)
