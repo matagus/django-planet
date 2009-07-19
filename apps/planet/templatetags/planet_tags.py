a7e784fdad1117cd0f6148b01a93826a38b2e9ef
Agustín Méndez <matagus@gmail.com>
2009-07-19 01:06:20 -0300

    Fix: Keep fixing things...

--- apps/planet/templatetags/planet_tags.py
+++ apps/planet/templatetags/planet_tags.py
@@ -6,9 +6,9 @@
 from django import template
 from django.db.models import Count, Max, F
 
-from planet.models import Author, Feed, Blog
+from planet.models import Author, Feed, Blog, Post
 
-from tagging.models import Tag
+from tagging.models import Tag, TaggedItem
 
 
 register = template.Library()
@@ -16,14 +16,20 @@ register = template.Library()
 
 @register.inclusion_tag('authors/blocks/list_for_tag.html')
 def authors_about(tag):
-    authors = Author.site_objects.filter(post=1).distinct()
+    post_ids = TaggedItem.objects.get_by_model(
+        Post.site_objects, tag).values_list("id", flat=True)
+    
+    authors = Author.site_objects.filter(post__in=post_ids).distinct()
 
     return {"authors": authors, "tag": tag}
 
 
 @register.inclusion_tag('feeds/blocks/list_for_tag.html')
 def feeds_about(tag):
-    feeds_list = Feed.site_objects.filter(post=1).distinct()
+    post_ids = TaggedItem.objects.get_by_model(
+        Post.site_objects, tag).values_list("id", flat=True)
+    
+    feeds_list = Feed.site_objects.filter(post__in=post_ids).distinct()
 
     return {"feeds_list": feeds_list, "tag": tag}
 
@@ -36,33 +42,12 @@ def related_tags_for(tag, count=20):
 
     return {"related_tags": related_tags, "max_posts_count": max_posts_count}
 
-
-@register.simple_tag
-def tags_count_for(instance):
-    if isinstance(instance, Feed):
-        return Tag.objects.filter(post__feed=instance
-            ).distinct().count()
-    elif isinstance(instance, Author):
-        return Tag.objects.filter(post__feed__author=instance
-            ).distinct().count()
-    else:
-        return 0
-
-
-@register.inclusion_tag("feeds/blocks/related_feeds.html")
-def related_feeds_for(feed, count=10):
-    feed_tags = Tag.objects.none() #filter(post__feed=feed).distinct()
-
-    related_feeds = []
-    if feed_tags:
-        related_feeds = Feed.objects.filter(post__tags__in=feed_tags
-            ).exclude(pk=feed.pk).distinct()[:count]
-
-    return {"related_feeds": related_feeds}
-
-
 @register.inclusion_tag("posts/details.html")
 def post_details(post):
+    return {"post": post}
+
+@register.inclusion_tag("posts/full_details.html")
+def post_full_details(post):
     return {"post": post}
     
 #@register.simple_tag
