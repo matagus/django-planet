df131a8f482e712546555e0cb28a58edcf960bf2
Steve Schwarz <sschwarz@sschwarz.(none)>
2010-01-24 01:08:45 +0800

    Print total number of posts added and total elapsed time

--- apps/planet/management/commands/update_all_feeds.py
+++ apps/planet/management/commands/update_all_feeds.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from datetime import datetime
 from django.core.management.base import BaseCommand
 
 from planet.management.commands import process_feed
@@ -17,8 +17,12 @@ class Command(BaseCommand):
     ./manage.py add_feed <feed_url>
     """
     def handle(self, *args, **options):
+        new_posts_count = 0
+        start = datetime.now()
         for feed_url in Feed.site_objects.all().values_list("url", flat=True):
             # process feed in create-mode
-            process_feed(feed_url, create=False)
+            new_posts_count += process_feed(feed_url, create=False)
+        delta = datetime.now() - start
+        print "Added %s posts in %d seconds" % (new_posts_count, delta.seconds)
         feeds_updated.send(sender=self, instance=self)
-                
+
