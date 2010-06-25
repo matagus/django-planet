78bcfa476d0a1160cd894376864c1a5fba8db0a2
matagus <matagus@gmail.com>
2010-06-24 22:24:42 -0300

    Fix: new setup file. Better description, media files removed from package and sample_project too.

--- setup.py
+++ setup.py
@@ -1,60 +1,43 @@
 # -*- coding: utf-8 -*-
-
-"""
-A nice example to make setup.py file can be found
-at http://jacobian.org/writing/django-apps-with-buildout/
-thanks to Jacob Kaplan-Moss.
-"""
-
-import os
 from setuptools import setup, find_packages
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(
-    name = "django-planet",
-    version = "0.1",
-    url = 'http://github.com/matagus/django-planet',
-    license = 'BSD',
-    description = "Django app to build a planet, RSS/Atom feeds aggregator.",
-    long_description = read('README'),
+    name="django-planet",
+    version=__import__("planet").__version__,
+    url="http://github.com/matagus/django-planet",
+    license="BSD",
+    description="Django app to build a planet, RSS/Atom feeds aggregator.",
+    long_description=open("docs/usage.rst").read(),
 
-    author = 'Matias Agustin Mendez',
-    author_email = 'me@matagus.com.ar',
+    author="Matias Agustin Mendez",
+    author_email="me@matagus.com.ar",
 
-    packages = find_packages(),
+    packages=find_packages(exclude=["sample_project"]),
+    package_dir={"planet": "planet"},
     package_data={
-        'planet': [
-            'templates/*.html',
-            'templates/planet/*.html',
-            'templates/planet/authors/*.html',
-            'templates/planet/authors/blocks/*.html',
-            'templates/planet/blogs/*.html',
-            'templates/planet/posts/*.html',
-            'templates/planet/tags/*.html',
-            'templates/planet/tags/blocks/*.html',
-            'templates/planet/feeds/*.html',
-            'templates/planet/feeds/blocks/*.html',
-            'templates/planet/microformats/*.xml',
-            'media/planet/css/*.css',
-            'media/planet/js/*.js',
-            'media/planet/icons/*.gif',
-            'media/planet/images/*.png',
+        "planet": [
+            "templates/*.html",
+            "templates/planet/*.html",
+            "templates/planet/*/*.html",
+            "templates/planet/authors/blocks/*.html",
+            "templates/planet/tags/blocks/*.html",
+            "templates/planet/feeds/blocks/*.html",
+            "media/planet/css/*.css",
+            "media/planet/js/*.js",
+            "media/planet/icons/*.gif",
+            "media/planet/images/*.png",
         ]},
 
-    install_requires = ['setuptools', 'django', 'feedparser', 
-        'django-pagination', 'django-tagging'],
-
-    classifiers = [
-        'Development Status :: 5 - Production/Stable',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary',
-    ]
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary",
+    ],
+    zip_safe=False,
 )
