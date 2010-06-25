3f5b7f18ae2f42046348b804567c9e0077e84aa7
matagus <matagus@gmail.com>
2010-06-24 22:24:25 -0300

    Fix:

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -47,7 +47,7 @@ USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, "planet/media/planet/")
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, "media/")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
