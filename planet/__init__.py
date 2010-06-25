57951ecdb7896a8b9e9379392bd2c5f26c453582
matagus <matagus@gmail.com>
2010-06-24 21:45:45 -0300

    Fix: planet.VERSION following PEP.

---
+++ planet/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+VERSION = (0, 1, 1, "a", 0) # following PEP 386
+
+def get_version():
+    version = "%s.%s" % (VERSION[0], VERSION[1])
+    if VERSION[2]:
+        version = "%s.%s" % (version, VERSION[2])
+    if VERSION[3] != "f":
+        version = "%s%s%s" % (version, VERSION[3], VERSION[4])
+    return version
+
+
+__version__ = get_version()
