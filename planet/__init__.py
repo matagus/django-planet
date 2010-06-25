6a68a9dfddf28eff837280832457f36802d8980f
matagus <matagus@gmail.com>
2010-06-25 17:53:10 -0300

    Fix: version incremented.

--- planet/__init__.py
+++ planet/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-VERSION = (0, 1, 1, "a", 0) # following PEP 386
+VERSION = (0, 1, 2, "a", 0) # following PEP 386
 
 def get_version():
     version = "%s.%s" % (VERSION[0], VERSION[1])
