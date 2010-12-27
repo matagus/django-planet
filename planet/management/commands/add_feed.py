30d1efbd38e1a4369972cf41d3b0a0b1db5fd836
Davide Setti <davide.setti@gmail.com>
2010-12-27 16:07:37 +0100

    add_feed can now add a feed directly into a category. Categories are used in FeedAdmin list

--- planet/management/commands/add_feed.py
+++ planet/management/commands/add_feed.py
@@ -2,13 +2,20 @@
 # -*- coding: utf-8 -*-
 
 from django.core.management.base import BaseCommand
-
 from planet.management.commands import process_feed
-
+from optparse import make_option
 
 class Command(BaseCommand):
     help = "Add a complete blog feed to our db."
     args = "<feed_url>"
+    option_list = BaseCommand.option_list + (
+        make_option('-c', '--category',
+            action='store',
+            dest='category',
+            default=None,
+            metavar='Title',
+            help='Add this feed to a Category'),
+        )
 
     def handle(self, *args, **options):
         if not len(args):
@@ -17,5 +24,5 @@ class Command(BaseCommand):
 
         feed_url = args[0]
         # process feed in create-mode
-        process_feed(feed_url, create=True)
+        process_feed(feed_url, create=True, category_title=options['category'])
                 
