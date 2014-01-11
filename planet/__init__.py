1e92684bbc24a4fecbb6d4e827660c070da5e3bc
Agustín Méndez <matagus@gmail.com>
2014-01-11 17:06:02 -0300

    Bump version to 0.4.12.

--- planet/__init__.py
+++ planet/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-VERSION = (0, 4, 11, "f") # following PEP 386
+VERSION = (0, 4, 12, "f") # following PEP 386
 
 def get_version():
     version = "%s.%s" % (VERSION[0], VERSION[1])
