12f3c426eec1f8b42667bd801ec0bb72fa0b073c
Agustin Mendez <matagus@gmail.com>
2015-12-19 18:44:40 -0300

    Adding missing migration for Author.email field

---
+++ planet/migrations/0002_auto_20151219_2141.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('planet', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='author',
+            name='email',
+            field=models.EmailField(db_index=True, max_length=254, verbose_name='Author email', blank=True),
+        ),
+    ]
